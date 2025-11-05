24e456f77ffe24b684a6f3ee29507f9e3810d4bd
Minh Nhật Nguyễn <155094042+DressAnDy@users.noreply.github.com>
2025-11-05 11:34:41 +0700

    Revert "Revert "Add Damage Report""

---
+++ src/EVStationRental.Repositories/IRepositories/IDamageReportRepository.cs
@@ -0,0 +1,24 @@
+﻿using EVStationRental.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVStationRental.Repositories.IRepositories
+{
+    public interface IDamageReportRepository
+    {
+        Task<List<DamageReport>> GetAllDamageReportsAsync();
+
+        Task<List<DamageReport>> GetDamageReportsByVehicleIdAsync(Guid vehicleId );
+        Task<DamageReport?> GetDamageReportByDamageIdAsync(Guid damageId);
+
+        Task<DamageReport?> GetDamageReportByOrderIdAsync(Guid orderId);
+        Task<DamageReport> CreateDamageReportAsync(DamageReport damageReport);
+        Task<bool> HardDeleteDamageReportAsync(Guid damageId);
+        Task<bool> SoftDeleteDamageReportAsync( Guid damageId );
+
+        Task<DamageReport?> UpdateDamageReportAsync(DamageReport damageReport);
+    }
+}
